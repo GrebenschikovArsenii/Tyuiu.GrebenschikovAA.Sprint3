6a975b849d30d2c49fc02acf875094516b45bc9a
BaimuhametovaMadina <fryecest@gmail.com>
2025-12-23 23:04:30 +0500

    task7

---
+++ Tyuiu.GrebenschikovAA.Sprint3.Task7.V27.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.GrebenschikovAA.Sprint3.Task7.V27.Lib
+{
+    public class DataService: ISprint3Task7V27
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] result = new double[11];
+            int index = 0;
+            for (int i = startValue; i <= stopValue; i++)
+            {
+                if (0 == (i + 1)) result[index] = 0;
+                else result[index] = Math.Round((Math.Cos(i) / (1 + i) - Math.Cos(i) * 1.3 + 3 * i), 2);
+                index++;
+            }
+            return result;
+        }
+    }
+}
---
+++ Tyuiu.GrebenschikovAA.Sprint3.Task7.V27/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.GrebenschikovAA.Sprint3.Task7.V27
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
